417614c61acbd389d7aab20f0e51ce6e63ef05f6
robbie <robbiekruszynski@gmail.com>
2020-10-26 21:30:35 -0700

    create item variables

--- app.js
+++ app.js
@@ -3,7 +3,16 @@
 const card = document.querySelector(".card");
 const container = document.querySelector(".container");
 
+//items
+const title = document.querySelector(".title");
+const nft = document.querySelector(".nft img");
+const purchase = document.querySelector(".purchase button");
+const description = document.querySelector(".info h3");
+const size = document.querySelector(".size");
+
 //moving animation
+
+// (e) = event listener
 
 container.addEventListener("mousemove", (e) => {
   let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
@@ -14,8 +23,12 @@ container.addEventListener("mousemove", (e) => {
 //pop card back to rest state
 
 //animate in
+container.addEventListener("mouseenter", (e) => {
+  card.style.transition = "none";
+});
 
 //animate out
 container.addEventListener("mouseleave", (e) => {
+  card.style.transition = "all 0.5s ease";
   card.style.transform = `rotateY(0deg) rotateX(0deg)`;
 });
