b90520177e310fd989855c9619e458f597e4a44e
robbie <robbiekruszynski@gmail.com>
2020-10-26 21:18:38 -0700

    add roate logic

---
+++ app.js
@@ -0,0 +1,12 @@
+//movement animation
+
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+
+//moving animation
+
+container.addEventListener("mousemove", (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+  let yAxis = (window.innerWidth / 2 - e.pageY) / 25;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
